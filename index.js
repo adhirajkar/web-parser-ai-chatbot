c3447a2ce78aa32287075b57692029aa3914873a
adhirajkar <adhirajkar3103@gmail.com>
2026-01-09 18:27:05 +0530

    added vector db

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ import * as cheerio from "cheerio";
 import OpenAI from "openai";
 import "dotenv/config";
 import { chunkText } from "./helper.js";
+import { ChromaClient } from "chromadb";
 
 const openai = new OpenAI();
+const chromaClient = new ChromaClient({
+  host: "localhost",
+  port: 8000,
+  ssl: false,
+});
+await chromaClient.heartbeat();
 
 async function scrapeWebpage(url = "") {
   const { data } = await axios.get(url);
@@ -13,39 +20,73 @@ async function scrapeWebpage(url = "") {
   const pageHead = $("head").html();
   const pageBody = $("body").html();
 
-  let internalLinks = [];
-  let externalLinks = [];
+  let internalLinks = new Set();
+  let externalLinks = new Set();
 
   $("a").each((_, ele) => {
     const link = $(ele).attr("href");
     if (link == "/") return;
     if (link.startsWith("https") || link.startsWith("https")) {
-      externalLinks.push(link);
+      externalLinks.add(link);
     } else {
-      internalLinks.push(link);
+      internalLinks.add(link);
     }
   });
 
-  return { head: pageHead, body: pageBody, internalLinks, externalLinks };
+  return {
+    head: pageHead,
+    body: pageBody,
+    internalLinks: Array.from(internalLinks),
+    externalLinks: Array.from(externalLinks),
+  };
 }
 
 async function generateVectorEmbeddings({ text }) {
   const embedding = await openai.embeddings.create({
     model: "text-embedding-3-small",
-    input: "Your text string goes here",
+    input: text,
     encoding_format: "float",
   });
   return embedding.data[0].embedding;
 }
 
+const WEB_COLLECTION = `WEB_SCRAPED_DATA`;
+async function insertIntoDb({ embedding, url, body = "", head }) {
+  const collection = await chromaClient.getOrCreateCollection({
+    name: WEB_COLLECTION,
+  });
+  await collection.add({
+    ids: [url],
+    embeddings: [embedding],
+    metadatas: [{ url, body, head }],
+  });
+}
 async function ingest(url = "") {
+  console.log("Ingesting ", url);
   const { head, body, internalLinks } = await scrapeWebpage(url);
-  const bodyChunks = chunkText(body, 2000);
+  const bodyChunks = chunkText(body, 1000);
 
-  const headEmbedding = await generateVectorEmbeddings({ text: head });
+  //   const headEmbedding = await generateVectorEmbeddings({ text: head });
+  //   await insertIntoDb({
+  //     embedding: headEmbedding,
+  //     url,
+  //   });
   for (const chunk of bodyChunks) {
     const bodyEmbedding = await generateVectorEmbeddings({ text: chunk });
+    await insertIntoDb({
+      embedding: bodyEmbedding,
+      url,
+      head,
+      body: chunk,
+    });
   }
+
+  for (const link of internalLinks) {
+    const _url = `${url}${link}`;
+    await ingest(_url);
+  }
+
+  console.log(`Ingesting success `, url);
 }
 
-scrapeWebpage("https://adhiraj.lol").then(console.log);
+ingest("https://adhiraj.lol");
